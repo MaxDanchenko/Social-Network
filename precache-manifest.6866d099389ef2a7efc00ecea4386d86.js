6591a5638e188ee06f3d4903517a4b0530e2d07d
Max Danchenko <max@Maxs-MacBook-Pro.local>
2021-01-20 11:20:44 +0200

    Updates

---
+++ precache-manifest.6866d099389ef2a7efc00ecea4386d86.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391ec77ee692e4efe4261e576b14fb50",
+    "url": "/Social-Network/index.html"
+  },
+  {
+    "revision": "d8a7cbc86fda6c9db888",
+    "url": "/Social-Network/static/css/main.3e6840a4.chunk.css"
+  },
+  {
+    "revision": "119a1b4ef4fc6e9bf6df",
+    "url": "/Social-Network/static/js/2.488e5844.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Social-Network/static/js/2.488e5844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a7cbc86fda6c9db888",
+    "url": "/Social-Network/static/js/main.0e5207e5.chunk.js"
+  },
+  {
+    "revision": "1ff006e4b26fc177c7b4",
+    "url": "/Social-Network/static/js/runtime-main.3828848e.js"
+  },
+  {
+    "revision": "c74b34ba21df944dfc2ce8ebaea905f3",
+    "url": "/Social-Network/static/media/Eclipse-1s-84px.c74b34ba.svg"
+  },
+  {
+    "revision": "689ea90f52b6991dc0bd718cdbef279b",
+    "url": "/Social-Network/static/media/annUser.689ea90f.jpg"
+  },
+  {
+    "revision": "5f8ab0b4177d65f1dd1119259ed9a1b0",
+    "url": "/Social-Network/static/media/reactBg.5f8ab0b4.jpg"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/Social-Network/static/media/reactLogo.876a8325.svg"
+  },
+  {
+    "revision": "59216aa3467af1b9307d4cdf3e1aa42e",
+    "url": "/Social-Network/static/media/wallpapperBg.59216aa3.jpg"
+  }
+]);
